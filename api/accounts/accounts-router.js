db7a530fa1c00e91fe3741f3d433855d64a538a5
bevanger <baileyevanger@gmail.com>
2021-09-06 19:11:47 -0600

    finished queries and added general endpoints

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,31 @@
+
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  res.json('returns an array of accounts (or an empty array if there aren't any)')
+});
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  res.json(' returns an account by the given id')
+});
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  res.json('returns the created account. Leading or trailing whitespace on budget name should be trimmed before saving to db.')
+});
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  res.json('returns the updated account. Leading or trailing whitespace on budget name should be trimmed before saving to db' )
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  res.json('returns the deleted account')
+});
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+  res.status(err.status || 500).json({
+    message: err.message,
+    customMessage: 'Something bad inside the accounts router'
+  })
+});
 
 module.exports = router;
